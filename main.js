79d4acfed18b504df0541a6e4e700b8d38bc9430
Dominik Wysota <Medutenza@gmail.com>
2019-10-10 12:33:59 +0200

    Add showResults and animation for it

--- main.js
+++ main.js
@@ -12,6 +12,76 @@ let selectedCountry = "";
 const typesMeasurements = ["pm25", "pm10", "so2", "no2", "o3", "co", "bc"];
 let measurements = "";
 
+//***Fuction check 10 largest count and save to new array***
+
+let activateAnimCheck = true;
+let backOrOut = false;
+
+//
+const animationsShowResults = () => {
+    const animationCheck = () => {
+        const sectionCheck = document.querySelector('.check');
+        var pos = 50;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (pos == 10) {
+                clearInterval(id);
+            } else {
+                pos--;
+                sectionCheck.style.marginTop = pos + 'vh';
+            }
+        }
+        activateAnimCheck = false;
+    }
+    if (activateAnimCheck === true) {
+        animationCheck();
+    }
+    setTimeout(function () {
+        document.querySelectorAll('.topDiv').forEach((item) => {
+            let pos = 100;
+            let id = setInterval(frame, 1);
+
+            function frame() {
+                if (pos == 0) {
+                    clearInterval(id);
+                } else {
+                    pos--;
+                    item.style.left = pos + '%';
+                }
+            }
+        })
+    }, 1500);
+    backOrOut = true;
+}
+
+const showResults = () => {
+    setTimeout(function () {
+        document.querySelectorAll('.topDiv h2').forEach((item, index) => {
+            item.textContent = arrayTenLargestMeas[index].city;
+        })
+        document.querySelectorAll('.topDiv p').forEach((item, index) => {
+            item.textContent = `${arrayTenLargestMeas[index].measurements[0].value.toFixed(2)} ${arrayTenLargestMeas[index].measurements[0].unit}`
+        })
+    }, 1500);
+    if (backOrOut) {
+        document.querySelectorAll('.topDiv').forEach((item) => {
+            let pos = 0;
+            let id = setInterval(frame, 1);
+
+            function frame() {
+                if (pos == 100) {
+                    clearInterval(id);
+                } else {
+                    pos++;
+                    item.style.left = pos + '%';
+                }
+            }
+        })
+    }
+    animationsShowResults();
+}
+
 const checkMeasurements = () => {
     fetch(`https://api.openaq.org/v1/latest?country=${selectedCountry}&limit=1000&parameter=${measurements}`)
         .then(resp => resp.json())
@@ -26,7 +96,6 @@ const checkMeasurements = () => {
                 arrayTenLargestCount[i] = resp.results[i];
             }
 
-            let h = 0; //Which index new array
             //Download the largest measurements with every city
             for (let i = 0; i < arrayTenLargestCount.length; i++) {
                 if (arrayTenLargestMeas.length >= 1) {
@@ -43,6 +112,7 @@ const checkMeasurements = () => {
                     arrayTenLargestMeas.push(arrayTenLargestCount[i]);
                 }
             }
+            showResults();
         });
 }
 
